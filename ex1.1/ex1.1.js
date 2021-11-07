0054148e065c24d0aed6aeaeaa7b8126a456a85e
newasad <85388826+newasad@users.noreply.github.com>
2021-11-07 11:02:49 +0200

    Create ex1.1.js

---
+++ ex1.1/ex1.1.js
@@ -0,0 +1,7 @@
+//node js hase a conection to file system in the mechin becuse node based on v8 engine witch is wrighting in c++
+//and read data from the cmd or terminal becuse node based on v8 engine witch is wrighting in c++ and hase a pross object
+//a conection to the operating system functionality 
+//and all this three are not possible in javascript
+
+
+//clent side javascript hase the window object and browsers forms and interaction with dom elements that is not available in node 
